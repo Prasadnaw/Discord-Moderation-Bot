9d2ddc92def515e72f204acae4683c978eec70c0
Prasadnaw <prasadnawkaryou@gmail.com>
2021-08-05 14:11:58 +0530

    sc

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = "your bot token";
+exports.TOKEN = "ODcwOTUwNTQ0NDg1NzkzODAz.YQUN5w.tOzDBKcIy_C68aFDmO21f6YHiao";
 
-exports.PREFIX = 'YOUR PREFIX';
+exports.PREFIX = '~';
 
-exports.OWNER_ID = "YOUR ACC ID"
+exports.OWNER_ID = "GamerPhase#9022"
